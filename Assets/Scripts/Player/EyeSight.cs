6ae15ab821b83b1502660e72bb7a23cb8c4eb96a
LeeHeungrok <rhh0208@naver.com>
2019-11-12 20:45:17 +0900

    [시야] 마우스에 따라 플레이어 머리 회전

--- Assets/Scripts/Player/EyeSight.cs
+++ Assets/Scripts/Player/EyeSight.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class EyeSight : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] GameObject player;
+    [SerializeField] GameObject head;
+
+    [SerializeField] bool isTracking = true;
+
+    void FixedUpdate()
     {
-        
+        TrackingMouse();
     }
 
-    // Update is called once per frame
-    void Update()
+    void TrackingMouse()
     {
-        
+        Vector3 cameraPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 playerPos = player.transform.position;
+        Vector2 mousePos = new Vector2(cameraPos.x - playerPos.x, cameraPos.y - playerPos.y);
+
+        float rad = Mathf.Atan2(mousePos.x, mousePos.y);
+        float mouseRotate = (rad * 180) / Mathf.PI;
+
+        head.transform.localEulerAngles = new Vector3(0, 0, (-mouseRotate + 90));
+    }
+
+    public void setTracking(bool tracking)
+    {
+        isTracking = tracking;
     }
 }
