5dd7ec1350eb22012d29cdddb2f7c6f43a21d14b
snrkgotdj <snrkgotdj@gmail.com>
2020-02-17 02:56:21 +0900

    셀레스트, 가속감속, 자잘한 이미지 붙여보기

---
+++ Assets/Collision.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    [Header("Layers")]
+    public LayerMask groundLayer;
+
+    [Space]
+
+    public bool onGround;
+    public bool onWall;
+    public bool onRightWall;
+    public bool onLeftWall;
+    public int wallSide;
+
+    [Space]
+
+    [Header("Collision")]
+
+    public float collisionRadius = 0.25f;
+    public Vector2 bottomOffset, rightOffset, leftOffset;
+    private Color debugCollisionColor = Color.red;
+
+    [Space]
+    
+    [Header("GroundRemember")]
+
+    float fJumpPressedRemember = 0;
+    [SerializeField]
+    float fJumpPressedRememberTime = 0.2f;
+
+    float fGroundedRemember = 0;
+    [SerializeField]
+    float fGroundedRememberTime = 0.25f;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        onGround = Physics2D.OverlapCircle((Vector2)transform.position + bottomOffset, collisionRadius, groundLayer);
+        fGroundedRemember -= Time.deltaTime;
+        if (onGround)
+        {
+            fGroundedRemember = fGroundedRememberTime;
+        }
+
+        if(fGroundedRemember > 0 && rb.velocity.y < 0)
+        {
+            onGround = true;
+        }
+
+        onRightWall = Physics2D.OverlapCircle((Vector2)transform.position + rightOffset, collisionRadius, groundLayer);
+        onLeftWall = Physics2D.OverlapCircle((Vector2)transform.position + leftOffset, collisionRadius, groundLayer);
+        onWall = onRightWall || onLeftWall;
+
+        wallSide = onRightWall ? -1 : 1;
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+
+        var positions = new Vector2[] { bottomOffset, rightOffset, leftOffset };
+
+        Gizmos.DrawWireSphere((Vector2)transform.position + bottomOffset, collisionRadius);
+        Gizmos.DrawWireSphere((Vector2)transform.position + rightOffset, collisionRadius);
+        Gizmos.DrawWireSphere((Vector2)transform.position + leftOffset, collisionRadius);
+    }
+}
