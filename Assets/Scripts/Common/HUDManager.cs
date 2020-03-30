59211e4e5feb57a54b914be8df7f635fd299b919
LeeHeungrok <rhh0208@naver.com>
2020-03-30 21:18:16 +0900

    [UI] HUD 수치에 따라 HP, Panic UI 변경되도록 구현

---
+++ Assets/Scripts/Common/HUDManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUDManager : MonoBehaviour
+{
+    private static HUDManager Instance = null;
+
+    [Header ("- 오브젝트 설정")]
+    [SerializeField] GameObject hpUI;
+    [SerializeField] GameObject panicBarUI;
+
+    private HUDManager() { }
+
+    void Start()
+    {
+        if (Instance == null)
+        {
+            GameObject.DontDestroyOnLoad(this);
+            Instance = this;
+        }
+        else
+        {
+            GameObject.Destroy(this);
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public static HUDManager GetInstance()
+    {
+        if (Instance == null)
+        {
+            Debug.Log("[WARNING] HUD Manager is null.");
+        }
+        return Instance;
+    }
+
+    public void RefreshHUD()
+    {
+        int hp = GameManager.GetInstance().getHP();
+        float panic = GameManager.GetInstance().getPanic();
+
+        for(int idx = 0; idx < hpUI.transform.childCount; ++idx)
+        {
+            bool isActive = idx < hp;
+
+            hpUI.transform.GetChild(idx).gameObject.SetActive(isActive);
+        }
+
+        panicBarUI.GetComponent<Image>().fillAmount = panic;
+    }
+}
