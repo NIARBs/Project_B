a409d4258a285330e1a90dbba8bb17853bba5e9f
snrkgotdj <snrkgotdj@gmail.com>
2020-04-04 05:11:16 +0900

    스크립트 폴더정리

--- Assets/Scripts/Movement/Movement.cs
+++ Assets/Scripts/Movement/Movement.cs
@@ -183,7 +183,6 @@ public class Movement : MonoBehaviour
 
     IEnumerator DashWait()
     {
-        FindObjectOfType<GhostTrail>().ShowGhost();
         StartCoroutine(GroundDash());
         DOVirtual.Float(14, 0, .8f, RigidbodyDrag);
 
